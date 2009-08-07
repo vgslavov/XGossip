42a1025830b0678c8466d793ea2657e645c097c7
vsfgd <empty>
2009-08-07 18:27:56 +0000

    can read sigs from sig files in specified dir save sigs to a vector of sigs generate a unique sig list and calc freq of each sig

--- chord-0.1-mod/tools/gpsi.C
+++ chord-0.1-mod/tools/gpsi.C
@@ -1,9 +1,11 @@
-/*	$Id$	*/
+/*	$Id: gpsi.C,v 1.1 2009/08/04 03:11:55 vsfgd Exp vsfgd $	*/
 
 #include <cmath>
 #include <ctime>
+#include <dirent.h>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -19,20 +21,20 @@
 #include "nodeparams.h"
 #include "gpsi.h"
 
-#define DEBUG 1
+#define _DEBUG_
+#define _ELIMINATE_DUP_
 
-//static char rcsid[] = "$Id$";
+//static char rcsid[] = "$Id: gpsi.C,v 1.1 2009/08/04 03:11:55 vsfgd Exp vsfgd $";
 extern char *__progname;
 
 dhashclient *dhash;
 int out;
 
 chordID maxID;
-static const char* dhash_sock;
+static const char* dsock;
 static const char* gsock;
-static const char* logfile;
-std::ofstream LogFile;
 
+//int pSim(std::vector<POLY>&, std::vector<POLY>&, std::vector<POLY>&, bool);
 void getKeyValue(str&, str&, double&, double&);
 void makeKeyValue(char **, int&, str&, double&, double&, InsertType);
 DHTStatus insertDHT(chordID, char *, int, int = MAXRETRIES, chordID = 0);
@@ -42,15 +44,24 @@ chordID randomID(void);
 void listen_gossip(void);
 void accept_connection(int);
 void read_gossip(int);
+int getdir(std::string, std::vector<std::string> &);
+void readsig(std::string);
+bool sig2str(std::vector<POLY>, str &);
+void calcfreq(void);
+void calcfreqO(void);
 void usage(void);
 
 DHTStatus insertStatus;
+std::string INDEXNAME;
 
 // entries of a node
 vec<str> nodeEntries;
 
 bool insertError;
 bool retrieveError;
+
+std::vector<std::vector<POLY> > sigList;
+std::map<std::vector<POLY>, int, CompareSig> uniqueSigList;
 
 vec<double> sum;
 //std::vector<double> sum;
@@ -63,6 +74,9 @@ int numReads;
 int numWrites;
 int numDocs;
 int cacheHits;
+
+// pSim
+bool metric = 1;
 
 // data transfered
 int dataFetched = 0, dataStored = 0;
@@ -166,21 +180,17 @@ int
 main(int argc, char *argv[])
 {
 	double s, w;
-	int ch, intval, fflag, Gflag, gflag, Lflag, lflag, Sflag, sflag, valLen;
-	int count, maxcount;
+	int ch, intval, fflag, Gflag, gflag, lflag, Sflag, sflag, valLen;
 	char *name;
 	char *value;
 	struct stat statbuf;
 	chordID ID;
 	DHTStatus status;
-	std::string sigfile, tagsfile;
-	FILE *sigfp;
-	FILE *tagsfp;
+	std::string sigdir;
+	std::vector<std::string> sigfiles;
 
-	fflag = Gflag = gflag = Lflag = lflag = Sflag = sflag = intval = 0;
-	// FIXME: add maxcount arg?
-	count = maxcount = 1;
-	while ((ch = getopt(argc, argv, "fG:ghi:L:lS:s:")) != -1)
+	fflag = Gflag = gflag = lflag = Sflag = sflag = intval = 0;
+	while ((ch = getopt(argc, argv, "fG:ghi:lS:s:")) != -1)
 		switch(ch) {
 		case 'f':
 			fflag = 1;
@@ -195,20 +205,16 @@ main(int argc, char *argv[])
 		case 'i':
 			intval = atoi(optarg);
 			break;
-		case 'L':
-			Lflag = 1;
-			logfile = optarg;
-			break;
 		case 'l':
 			lflag = 1;
 			break;
 		case 'S':
 			Sflag = 1;
-			dhash_sock = optarg;
+			dsock = optarg;
 			break;
 		case 's':
 			sflag = 1;
-			sigfile = optarg;
+			sigdir = optarg;
 			break;
 		case 'h':
 		case '?':
@@ -220,7 +226,7 @@ main(int argc, char *argv[])
 	argv += optind;
 
 	// required flags
-	if (Gflag == 0 || Sflag == 0 || Lflag == 0 || sflag == 0) usage();
+	if (Gflag == 0 || Sflag == 0 || sflag == 0) usage();
 
 	// FIXME: make sure only one flag is set at a time
 	if (fflag == 0 && gflag == 0 && lflag == 0) usage();
@@ -228,20 +234,17 @@ main(int argc, char *argv[])
 	// check intval bound
 	if (gflag == 1 && intval == 0) usage();
 
-	// check
-	LogFile.open(logfile);
+	if (stat(dsock, &statbuf) != 0) fatal("socket does not exist\n");
+	dhash = New dhashclient(dsock);
 
-	// open signatures
-	sigfp = fopen(sigfile.c_str(), "r");
-	assert(sigfp);
+	getdir(sigdir, sigfiles);
+	sigList.clear();
+	for (unsigned int i = 0; i < sigfiles.size(); i++) {
+		std::cout << sigfiles[i] << "\n";
+		readsig(sigfiles[i]);
+	}
 
-	// open tags
-	tagsfile = sigfile + std::string(TAGFILE);
-	tagsfp = fopen(tagsfile.c_str(), "r");
-	assert(tagsfp);
-
-	if (stat(dhash_sock, &statbuf) != 0) fatal("socket does not exist\n");
-	dhash = New dhashclient(dhash_sock);
+	calcfreq();
 
 	system("date");
 
@@ -511,9 +514,396 @@ read_gossip(int fd)
 	// check if sum and weight vecs have the same size?
 }
 
+// based on:
+// http://www.linuxquestions.org/questions/programming-9/c-list-files-in-directory-379323/
+int
+getdir(std::string dir, std::vector<std::string> &files)
+{
+	DIR *dp;
+	struct dirent *dirp;
+
+	if ((dp = opendir(dir.c_str())) == NULL) {
+		fatal("can't read dir\n");
+		return errno;
+	}
+	while ((dirp = readdir(dp)) != NULL) {
+		if (strcmp(dirp->d_name, ".") == 0 || strcmp(dirp->d_name, "..") == 0) continue;
+		files.push_back(dir + std::string(dirp->d_name));
+	}
+	closedir(dp);
+	return 0;
+}
+
+void
+readsig(std::string sigfile)
+{
+	double startTime, finishTime;
+	FILE *sigfp;
+	//FILE *tagsfp;
+	//std::string tagsfile;
+	chordID rootNodeID;
+
+	startTime = getgtod();
+	// open signatures
+	warnx << "opening sigfile: " << sigfile.c_str() << "...\n";
+	sigfp = fopen(sigfile.c_str(), "r");
+	// change to if?
+	assert(sigfp);
+
+	// open tags
+	//tagsfile = sigfile + std::string(TAGFILE);
+	//warnx << "opening tagsfile: " << tagsfile.c_str() << "...\n";
+	//tagsfp = fopen(tagsfile.c_str(), "r");
+	// change to if?
+	//assert(tagsfp);
+
+	// TODO: add maxcount arg?
+
+	// DONT use readData to retrieve signatures from input files...
+	// since the size filed uses POLY as a basic unit and not byte...
+	// Read numSigs <it should be 1> for data signatures...
+	int numSigs;
+	if (fread(&numSigs, sizeof(int), 1, sigfp) != 1) {
+		warnx << "numSigs: " << numSigs << "\n";
+	}
+	warnx << "NUM sigs: " << numSigs << "\n";
+	assert(numSigs == 1);
+
+	int size;
+
+	if (fread(&size, sizeof(int), 1, sigfp) != 1) {
+		assert(0);
+	}
+	warnx << "Signature size: " << size << "\n";
+
+	std::vector<POLY> sig;
+	sig.clear();
+	POLY e;
+	std::cout << "Document signature: ";
+	for (int i = 0; i < size; i++) {
+		if (fread(&e, sizeof(POLY), 1, sigfp) != 1) {
+			assert(0);
+		}
+		sig.push_back(e);
+		std::cout << e << ".";
+	}
+	std::cout << "\n\n";
+	sigList.push_back(sig);
+	warnx << "Signature list size: " << sigList.size() << "\n\n";
+	finishTime = getgtod();
+	fclose(sigfp);
+
+	/*
+	char hostname[64];
+	gethostname(hostname, sizeof(hostname));
+#ifdef _LIVE_
+	strcpy(hostname, "");
+#endif
+	// TODO: add docidType arg?
+	char docId[128];
+	// Read the document id
+	int len;
+	// len of docid string includes \0
+	if (fread(&len, sizeof(len), 1, sigfp) != 1) {
+		assert(0);
+	}
+
+	if (fread(&docId, len, 1, sigfp) != 1) {
+		assert(0);
+	}
+	// XXX: add the hostname
+	strcat(docId, ".");
+	strcat(docId, hostname);
+	strcat(docId, "#");
+
+	//warnx << "**********************************************************\n";
+	//warnx << "*            Inserting document " << docId << " ********\n";
+	//warnx << "**********************************************************\n";
+
+	// Read the distinct tags
+	std::vector<std::string> distinctTags;
+	distinctTags.clear();
+
+	readTags(tagsfp, distinctTags);
+
+	// Each H-index based on tagname!
+	for (int ts = 0; ts < (int) distinctTags.size(); ts++) {
+		// Do not insert for attributes
+		if (distinctTags[ts].find(ATTRSUFFIX) != std::string::npos) {
+			continue;
+		}
+		INDEXNAME = distinctTags[ts];
+
+		// is REPLICAID needed?
+		std::string root = INDEXNAME + std::string(ROOTNODEID);
+		//std::string root = INDEXNAME + std::string(ROOTNODEID) + std::string(REPLICAID);
+		//warnx << "********* H-index chosen for inserting signature:  " << distinctTags[ts].c_str() << " ********** \n";
+		rootNodeID = compute_hash(root.c_str(), root.length());
+		//warnx << "Root node id for index " << INDEXNAME.c_str() << " : " << rootNodeID << "\n";
+
+		// ROOT node id			
+		strbuf s;
+		s << rootNodeID;
+		str rootID(s);
+
+		//warnx << "ROOT ID: " << rootID << "\n";
+#ifdef _DEBUG_
+		for (int l = 0; l < (int) sig.size(); l++) {
+			warnx << sig[l] << " ";
+		}
+		//warnx << "\n";
+#endif
+	}
+	fclose(sigfp);
+	fclose(tagsfp);
+
+	//warnx << "Data read in bytes: " << dataFetched << "\n";
+	//warnx << "Data written in bytes: " << dataStored << "\n";
+	//warnx << "Number of documents inserted: " << 1 << "\n";
+	// TODO: count?
+	//warnx << "Number of documents inserted: " << count - 1 << "\n";
+	//warnx << "Number of full nodes: " << cacheOfFullNodes.size() << "\n";
+	finishTime = getgtod();
+	//std::cerr << "Time taken to process XML tags: " << finishTime - startTime << " secs " << std::endl;
+	*/
+}
+
+bool
+sig2str(std::vector<POLY> sig, str &buf)
+{
+	strbuf s;
+
+	if (sig.size() <= 0) return false;
+
+	for (int i = 0; i < (int) sig.size(); i++)
+		s << sig[i] << ".";
+
+	buf = s;
+	return true;
+}
+
+void
+calcfreq(void)
+{
+	str buf;
+
+	for (int i = 0; i < (int) sigList.size(); i++) {
+		std::map<std::vector<POLY>, int >::iterator itr = uniqueSigList.find(sigList[i]);
+		sig2str(sigList[i], buf);
+		if (itr != uniqueSigList.end()) {
+			itr->second += 1;
+			warnx << "sig: " << buf << "\nfreq: " << itr->second << "\n";
+		} else {
+			uniqueSigList[sigList[i]] = 1;
+			warnx << "sig: " << buf << "\nfreq: " << uniqueSigList[sigList[i]] << "\n";
+		}
+		warnx << "Size of unique sig list: " << uniqueSigList.size() << "\n\n";
+	}
+}
+
+// copied from psi.C
+void
+calcfreqO(void)
+{
+	std::vector<int> group1;
+	std::vector<int> group2;
+	std::vector<POLY> lcm1;
+	std::vector<POLY> lcm2;
+	std::multimap<int, int> sortedSigList;
+
+#ifdef _ELIMINATE_DUP_
+	std::map<std::vector<POLY>, std::vector<int>, CompareSig> uniqueSigList;
+	std::map<int, std::map<std::vector<POLY>, std::vector<int>, CompareSig>::const_iterator > duplicateList;
+
+	// Sort them based on degree
+	for (int i = 0; i < (int) sigList.size(); i++) {
+		std::map<std::vector<POLY>, std::vector<int> >::iterator itr = uniqueSigList.find(sigList[i]);
+		if (itr != uniqueSigList.end()) {
+			itr->second.push_back(i+1);
+		} else {
+			int deg = getDegree(sigList[i]);
+			//warnx << "Check degree: " << deg << "\n";
+			sortedSigList.insert(std::pair<int, int>(deg, i));
+			std::vector<int> e;
+			e.clear();
+			e.push_back(i+1);
+			uniqueSigList[sigList[i]] = e;
+
+			std::map<std::vector<POLY>, std::vector<int>, CompareSig>:: const_iterator uitr = 
+			uniqueSigList.find(sigList[i]);
+			//warnx << "Size of unique sig list: " << uniqueSigList.size() << "\n";
+			assert(uitr != uniqueSigList.end());
+
+			duplicateList[i+1] = uniqueSigList.find(sigList[i]);
+		}
+	}
+	assert(uniqueSigList.size() > 0);
+
+	// If all are identical
+	if (uniqueSigList.size() == 1) {
+		for (int i = 0; i < (int) floor(MAXENTRIES/2.0); i++) {
+			group1.push_back(i+1);
+		}
+		lcm1 = uniqueSigList.begin()->first;
+
+		for (int i = (int) floor(MAXENTRIES/2.0); i < MAXENTRIES; i++) {
+			group2.push_back(i+1);
+		}
+		lcm2 = uniqueSigList.begin()->first;
+		return;
+	}
+	warnx << "Size of unique sig list: " << uniqueSigList.size() << "\n";
+	warnx << "Size of dup sig list: " << duplicateList.size() << "\n";
+#else
+	// Sort them based on degree
+	for (int i = 0; i < (int) sigList.size(); i++) {
+		int deg = getDegree(sigList[i]);
+		//warnx << "Check degree: " << deg << "\n";
+		sortedSigList.insert(std::pair<int, int>(deg, i));
+	}
+#endif
+
+	warnx << "Size of sorted sig list: " << sortedSigList.size() << "\n";
+
+	int seed1 = -1, seed2 = -1;
+	std::vector<POLY> gcdPoly;
+	int minDeg = INT_MAX;
+
+	// Find the two most dissimilar
+	std::multimap<int, int>::reverse_iterator endItrI = sortedSigList.rbegin();
+	int i = 0;
+	int MAXSIGS = 10;
+	// Adjust the last sig to be checked
+	while (i < MAXSIGS && endItrI != sortedSigList.rend()) {
+		endItrI++;
+		i++;
+	}		
+
+	for (std::multimap<int, int>::reverse_iterator itrI = sortedSigList.rbegin();
+	     itrI != endItrI; itrI++) {
+
+		std::multimap<int, int>::reverse_iterator itrJ = itrI;
+		itrJ++;
+
+		for (; itrJ != endItrI; itrJ++) {
+			gcdPoly.clear();
+			gcdSpecial(gcdPoly, sigList[itrI->second], sigList[itrJ->second]);
+			std::vector<POLY> mylcm;
+			mylcm = sigList[itrI->second];
+
+			//int deg = pSim(sigList[itrI->second], sigList[itrJ->second], gcdPoly,
+			//    metric);
+			int deg = pSimOpt(mylcm, sigList[itrJ->second], gcdPoly, metric);
+			//warnx << "Degree: " << deg << "\n";
+			if (deg < minDeg) {
+				seed1 = itrI->second;
+				seed2 = itrJ->second;
+				minDeg = deg;
+			}
+
+			if (deg < 0) break;
+		}
+	}
+
+	//warnx << "MINDEGREE: " << minDeg << "\n";
+	group1.push_back(seed1+1);
+	group2.push_back(seed2+1);
+
+	//warnx << "SEED1 : " << seed1
+	//      << "  SEED2 : " << seed2
+	//      << "\n";
+	lcm1 = sigList[seed1];
+	lcm2 = sigList[seed2];
+
+	std::vector<POLY> gcd1, gcd2;
+	std::vector<POLY> testLCM1, testLCM2;
+
+	for (std::multimap<int, int>::iterator itr = sortedSigList.begin();
+	     itr != sortedSigList.end(); itr++) {
+		if (itr->second == seed1 || itr->second == seed2) continue;
+
+		gcd1.clear();
+		gcd2.clear();
+
+		gcdSpecial(gcd1, lcm1, sigList[itr->second]);
+		gcdSpecial(gcd2, lcm2, sigList[itr->second]);
+
+		testLCM1 = lcm1;
+		testLCM2 = lcm2;
+
+		//int deg1 = getDegree(gcd1);
+		//int deg2 = getDegree(gcd2);
+		int deg1 = pSimOpt(testLCM1, sigList[itr->second], gcd1, metric);
+		int deg2 = pSimOpt(testLCM2, sigList[itr->second], gcd2, metric);
+
+		if (deg1 > deg2) {
+			//lcm(lcm1, sigList[itr->second]);
+			lcm1 = testLCM1;
+			group1.push_back(itr->second+1);
+		}
+		else if (deg1 < deg2) {
+			//lcm(lcm2, sigList[itr->second]);
+			lcm2 = testLCM2;
+			group2.push_back(itr->second+1);
+		} else {
+			if (group1.size() <= group2.size()) {
+				//lcm(lcm1, sigList[itr->second]);
+				lcm1 = testLCM1;
+				group1.push_back(itr->second+1);
+			} else {
+				//lcm(lcm2, sigList[itr->second]);
+				lcm2 = testLCM2;
+				group2.push_back(itr->second+1);
+			}
+		}		
+	}
+
+#ifdef _ELIMINATE_DUP_
+	int group1Size = group1.size();
+	int group2Size = group2.size();
+	warnx << "Group1 size: " << group1Size << "\n";
+
+	for (int i = 0; i < group1Size; i++) {
+		std::map<int, std::map<std::vector<POLY>, std::vector<int>, CompareSig>::const_iterator >::const_iterator itr = 
+		duplicateList.find(group1[i]);
+
+		assert(itr != duplicateList.end());
+
+		std::map<std::vector<POLY>, std::vector<int>, CompareSig>::const_iterator ditr = itr->second;
+		//warnx << "--> " << ditr->second.size() << "\n";
+		// Skip the first entry -- already in group1
+		for (int k = 1; k < (int) ditr->second.size(); k++) {
+			group1.push_back(ditr->second[k]);	
+		}
+	}
+
+	warnx << "Group2 size: " << group2Size << "\n";
+
+	for (int i = 0; i < group2Size; i++) {
+		std::map<int, std::map<std::vector<POLY>, std::vector<int>, CompareSig>::const_iterator >::const_iterator itr = 
+		duplicateList.find(group2[i]);
+
+		assert(itr != duplicateList.end());
+
+		std::map<std::vector<POLY>, std::vector<int>, CompareSig>::const_iterator ditr = itr->second;
+
+		//warnx << "--> " << ditr->second.size() << "\n";
+		// Skip the first entry -- already in group2
+		for (int k = 1; k < (int) ditr->second.size(); k++) {
+			group2.push_back(ditr->second[k]);	
+		}
+	}
+	warnx << "Sig list size: " << sigList.size() << "\nTotal size: " << group1.size() + group2.size() << "\n";
+	// Header is absent - one less
+	//assert(group1.size() + group2.size() == (size_t) MAXENTRIES);
+#endif
+
+	return;
+}
+
 void
 usage(void)
 {
-	warn << "usage: " << __progname << " [-h] [-S dhash-sock] [-G g-sock] [-L logfile] [-s sigfile] [-f | -l | -g [-i interval in sec]]\n";
+	warn << "usage: " << __progname << " [-h] [-S dhash-sock] [-G g-sock] [-s sigdir] [-f | -l | -g [-i interval in sec]]\n";
 	exit(0);
 }
